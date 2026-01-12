c259593ce1dc7dbfdd4b4b1248a07018d512b216
nathaniel-daniel <80441888+nathaniel-daniel@users.noreply.github.com>
2026-01-11 23:50:52 -0800

    Add json output for cli info subcommand (#14)
    * Use token from config when possible for info fetching
    
    * Add json output format for cli info subcommand

--- gofile-cli/src/commands/info.rs
+++ gofile-cli/src/commands/info.rs
@@ -1,20 +1,30 @@
+use crate::Config;
 use crate::util::parse_page_url;
 use anyhow::Context;
 use url::Url;
+
+#[derive(Debug, Default, Copy, Clone, clap::ValueEnum)]
+pub enum OutputFormat {
+    #[default]
+    Human,
+
+    Json,
+}
 
 #[derive(Debug, clap::Parser)]
 #[command(about = "Get the info from a https://gofile.io link")]
 pub struct Options {
     pub url: String,
+
+    #[arg(
+        long = "output-format",
+        default_value_t = Default::default(),
+        value_enum,
+    )]
+    pub output_format: OutputFormat,
 }
 
-pub async fn exec(client: gofile::Client, options: Options) -> anyhow::Result<()> {
-    let url = Url::parse(&options.url)?;
-    let id = parse_page_url(&url)?;
-
-    client.login_guest().await?;
-
-    let page = client.get_page(id).await.context("failed to get page")?;
+fn output_human(page: &gofile::Page) {
     for (id, page) in page.children.iter() {
         println!("Id: {id}");
         println!("Name: {}", page.name);
@@ -33,6 +43,31 @@ pub async fn exec(client: gofile::Client, options: Options) -> anyhow::Result<()
             println!("Link: {link}");
         }
         println!();
+    }
+}
+
+fn output_json(page: &gofile::Page) -> anyhow::Result<()> {
+    let stdout = std::io::stdout();
+    let lock = stdout.lock();
+    serde_json::to_writer(lock, page)?;
+    Ok(())
+}
+
+pub async fn exec(client: gofile::Client, options: Options) -> anyhow::Result<()> {
+    let config = Config::load().context("failed to load config")?;
+
+    let url = Url::parse(&options.url)?;
+    let id = parse_page_url(&url)?;
+
+    match config.as_ref().and_then(|config| config.token.as_ref()) {
+        Some(token) => client.set_token(token.clone()),
+        None => client.login_guest().await?,
+    }
+
+    let page = client.get_page(id).await.context("failed to get page")?;
+    match options.output_format {
+        OutputFormat::Human => output_human(&page),
+        OutputFormat::Json => output_json(&page)?,
     }
 
     Ok(())

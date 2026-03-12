32c09d39c0caa51ab5ade1afde56ca6dba692d11
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2026-03-12 14:46:00 -0700

    Add reference

---
+++ reference/generate-wt.2026.03.12.js
@@ -0,0 +1,46 @@
+function generateWT(_0x2ca17b) {
+    var _0x26f7d6 = a0_0x5892,
+    _0xfdd7a5 = {
+        '\x43\x4d\x54\x55\x7a': _0x26f7d6(0x7a, '\x74\x4f\x62\x61'),
+        '\x6e\x77\x74\x43\x6b': function (_0x1275d8, _0x8c41ba) {
+            return _0x1275d8 / _0x8c41ba;
+        },
+        '\x52\x6b\x4a\x77\x56': function (_0x1fe660, _0x430580) {
+            return _0x1fe660 / _0x430580;
+        },
+        '\x77\x79\x41\x42\x48': function (_0x582a19, _0xce4d51) {
+            return _0x582a19 + _0xce4d51;
+        },
+        '\x68\x53\x6a\x65\x78': function (_0x267406, _0x22f33b) {
+            return _0x267406 + _0x22f33b;
+        },
+        '\x61\x46\x65\x69\x61': function (_0x15b572, _0x20407b) {
+            return _0x15b572 + _0x20407b;
+        },
+        '\x4d\x64\x52\x66\x78': _0x26f7d6(0xbf, '\x40\x68\x5b\x59'),
+        '\x4b\x53\x41\x59\x6d': function (_0x4d0e96, _0x49c884) {
+            return _0x4d0e96(_0x49c884);
+        }
+    },
+    _0x40e4c3 = _0xfdd7a5['\x43\x4d\x54\x55\x7a'][_0x26f7d6(0xe2, '\x69\x36\x24\x6e')]('\x7c'),
+    _0xf45e03 = 0x0;
+    while (!![]) {
+        switch (_0x40e4c3[_0xf45e03++]) {
+        case '\x30':
+            var _0x527d1a = navigator[_0x26f7d6(0xde, '\x56\x70\x5a\x5e')];
+            continue;
+        case '\x31':
+            var _0x1bcefc = Math[_0x26f7d6(0x129, '\x56\x70\x5a\x5e')](_0xfdd7a5['\x6e\x77\x74\x43\x6b'](_0xfdd7a5[_0x26f7d6(0x106, '\x25\x49\x31\x58')](Date[_0x26f7d6(0x164, '\x56\x6a\x5b\x41')](), 0x3e8), 0x3840))[_0x26f7d6(0x119, '\x5a\x64\x62\x78')]();
+            continue;
+        case '\x32':
+            var _0x29116f = _0xfdd7a5[_0x26f7d6(0xe7, '\x74\x4f\x62\x61')](_0xfdd7a5['\x77\x79\x41\x42\x48'](_0xfdd7a5['\x77\x79\x41\x42\x48'](_0xfdd7a5[_0x26f7d6(0xed, '\x34\x34\x65\x5e')](_0xfdd7a5[_0x26f7d6(0x178, '\x32\x76\x77\x44')](_0xfdd7a5['\x61\x46\x65\x69\x61'](_0x527d1a, '\x3a\x3a') + _0xba68bc, '\x3a\x3a'), _0x2ca17b), '\x3a\x3a'), _0x1bcefc) + '\x3a\x3a', _0xfdd7a5[_0x26f7d6(0x130, '\x66\x46\x6b\x4e')]);
+            continue;
+        case '\x33':
+            return _0xfdd7a5[_0x26f7d6(0x157, '\x5d\x40\x54\x53')](_sha256, _0x29116f);
+        case '\x34':
+            var _0xba68bc = navigator[_0x26f7d6(0xec, '\x65\x62\x29\x43')] || '';
+            continue;
+        }
+        break;
+    }
+}

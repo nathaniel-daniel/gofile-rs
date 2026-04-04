8d65cbb5c574ad892b6e7ec0ceb0f425ab3b2584
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2026-04-03 22:47:43 -0700

    Fix website token generation

--- gofile/src/client.rs
+++ gofile/src/client.rs
@@ -11,6 +11,7 @@ use sha2::Sha256;
 use std::sync::Arc;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
+use tokio::sync::Semaphore;
 
 const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36";
 const LANGUAGE: &str = "en-US";
@@ -23,6 +24,7 @@ fn unix_epoch_secs() -> f64 {
 }
 
 /// See: https://github.com/yt-dlp/yt-dlp/issues/16117#issuecomment-4011609858
+#[expect(dead_code)]
 fn generate_website_token(token: &str) -> String {
     let time_4 = (unix_epoch_secs() / f64::from(60 * 60 * 4)) as u64;
     let hash_salt = "gf2026x";
@@ -36,10 +38,17 @@ fn generate_website_token(token: &str) -> String {
     base16ct::lower::encode_string(&hash)
 }
 
+#[derive(Debug)]
+struct Tokens {
+    token: Option<String>,
+    website_token: Option<String>,
+}
+
 /// The client state
 #[derive(Debug)]
 struct ClientState {
-    token: std::sync::Mutex<Option<(String, String)>>,
+    tokens: std::sync::Mutex<Tokens>,
+    get_website_token_sem: Semaphore,
 }
 
 /// A client
@@ -60,25 +69,29 @@ impl Client {
             .build()
             .expect("failed to build client");
         let state = Arc::new(ClientState {
-            token: std::sync::Mutex::new(None),
+            tokens: std::sync::Mutex::new(Tokens {
+                token: None,
+                website_token: None,
+            }),
+            get_website_token_sem: Semaphore::new(1),
         });
         Self { client, state }
     }
 
     /// Set the token.
     pub fn set_token(&self, token: String) {
-        let website_token = generate_website_token(&token);
-        *self.state.token.lock().expect("token poisoned") = Some((token, website_token));
+        self.state.tokens.lock().expect("tokens poisoned").token = Some(token);
     }
 
-    /// Get the token and website token.
+    /// Get the token.
     ///
     /// Should not need to be used under normal circumstances.
-    pub fn get_tokens(&self) -> Result<(String, String), Error> {
+    pub fn get_token(&self) -> Result<String, Error> {
         self.state
-            .token
+            .tokens
             .lock()
             .expect("token poisoned")
+            .token
             .clone()
             .ok_or(Error::MissingToken)
     }
@@ -100,9 +113,76 @@ impl Client {
         Ok(())
     }
 
+    async fn get_website_token(&self) -> Result<String, Error> {
+        let permit = self
+            .state
+            .get_website_token_sem
+            .acquire()
+            .await
+            .expect("sem closed");
+        let (token, maybe_website_token) = {
+            let tokens = self.state.tokens.lock().expect("tokens poisoned");
+
+            (
+                tokens.token.clone().ok_or(Error::MissingToken)?,
+                tokens.website_token.clone(),
+            )
+        };
+        if let Some(website_token) = maybe_website_token {
+            return Ok(website_token);
+        }
+
+        let script = self
+            .client
+            .get("https://gofile.io/dist/js/wt.obf.js")
+            .send()
+            .await?
+            .error_for_status()?
+            .text()
+            .await?;
+
+        let website_token = tokio::task::spawn_blocking(move || {
+            let runtime = rquickjs::Runtime::new()?;
+            let ctx = rquickjs::Context::full(&runtime)?;
+
+            let website_token = ctx.with(|ctx| {
+                let globals = ctx.globals();
+                let navigator = rquickjs::Object::new(ctx.clone())?;
+                navigator.set("userAgent", USER_AGENT)?;
+                navigator.set("language", LANGUAGE)?;
+
+                globals.set("navigator", navigator)?;
+
+                let result: Result<(), _> = ctx.eval(script);
+                result?;
+
+                let result: Result<String, _> = ctx.eval(format!("generateWT(\"{token}\")"));
+                let website_token = result?;
+
+                Ok::<_, Error>(website_token)
+            })?;
+
+            Ok::<_, Error>(website_token)
+        })
+        .await??;
+
+        self.state
+            .tokens
+            .lock()
+            .expect("tokens poisoned")
+            .website_token = Some(website_token.clone());
+
+        drop(permit);
+
+        Ok(website_token)
+    }
+
     /// Get a page.
     pub async fn get_page(&self, id: &str) -> Result<Page, Error> {
-        let (token, website_token) = self.get_tokens()?;
+        // TODO: We lock client data twice here, we should probably only do so once.
+        let token = self.get_token()?;
+        let website_token = self.get_website_token().await?;
+
         let page = 1;
         let page_size = 1000;
         let url = format!(
@@ -128,7 +208,7 @@ impl Client {
         let form = Form::new().part("file", file);
 
         let url = "https://upload.gofile.io/uploadfile";
-        let (token, _) = self.get_tokens()?;
+        let token = self.get_token()?;
         let api_response: ApiResponse<UploadInfo> = self
             .client
             .post(url)
